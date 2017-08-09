e0b6c742643fbdfe293ee6369d813befad270cbd
kwpark <pkwworld11@gmail.com>
2017-08-09 19:02:10 +0900

    MemoPad upload

---
+++ MemoPad/memo/memo/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace memo
+{
+    partial class memoPadForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.file = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPage = new System.Windows.Forms.ToolStripMenuItem();
+            this.open = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.ToolStripMenuItem();
+            this.anotherName = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.help = new System.Windows.Forms.ToolStripMenuItem();
+            this.memoPadInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.content = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file,
+            this.help});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1177, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // file
+            // 
+            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newPage,
+            this.open,
+            this.save,
+            this.anotherName,
+            this.toolStripSeparator1,
+            this.exit});
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(60, 29);
+            this.file.Text = "파일";
+            // 
+            // newPage
+            // 
+            this.newPage.Name = "newPage";
+            this.newPage.Size = new System.Drawing.Size(253, 30);
+            this.newPage.Text = "새로만들기";
+            this.newPage.Click += new System.EventHandler(this.newPage_Click);
+            // 
+            // open
+            // 
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(253, 30);
+            this.open.Text = "열기";
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // save
+            // 
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(253, 30);
+            this.save.Text = "저장";
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // anotherName
+            // 
+            this.anotherName.Name = "anotherName";
+            this.anotherName.Size = new System.Drawing.Size(253, 30);
+            this.anotherName.Text = "다른 이름으로 저장";
+            this.anotherName.Click += new System.EventHandler(this.anotherName_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(250, 6);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(253, 30);
+            this.exit.Text = "끝내기";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // help
+            // 
+            this.help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.memoPadInfo});
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(78, 29);
+            this.help.Text = "도움말";
+            // 
+            // memoPadInfo
+            // 
+            this.memoPadInfo.Name = "memoPadInfo";
+            this.memoPadInfo.Size = new System.Drawing.Size(211, 30);
+            this.memoPadInfo.Text = "메모장 정보";
+            this.memoPadInfo.Click += new System.EventHandler(this.memoPadInfo_Click);
+            // 
+            // content
+            // 
+            this.content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.content.Location = new System.Drawing.Point(0, 33);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(1177, 740);
+            this.content.TabIndex = 1;
+            this.content.TextChanged += new System.EventHandler(this.content_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // memoPadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 773);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "memoPadForm";
+            this.Text = "Kwpark MemoPad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem file;
+        private System.Windows.Forms.ToolStripMenuItem newPage;
+        private System.Windows.Forms.ToolStripMenuItem open;
+        private System.Windows.Forms.ToolStripMenuItem save;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem help;
+        private System.Windows.Forms.ToolStripMenuItem memoPadInfo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.TextBox content;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem anotherName;
+    }
+}
+
---
+++ MemoPad/memo/memo/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memo
+{
+    public partial class memoPadForm : Form
+    {
+        public bool modified = false;
+        public bool haveFileName = false;
+        public string filePath = string.Empty;
+
+
+        public memoPadForm()
+        {
+            InitializeComponent();
+        }
+
+        public void reset_memo()
+        {
+            content.Text = string.Empty;
+            filePath = string.Empty;
+            modified = false;
+            haveFileName = false;
+            memoPadForm.ActiveForm.Text = "제목 없음 - 메모장";
+        }
+
+        public string getFileName()
+        {
+            int startFileNamePosition = filePath.LastIndexOf("\\");
+            int endFileNamePosition = filePath.LastIndexOf(".");
+            int nameLenth = (endFileNamePosition - 1) - startFileNamePosition;
+            string name = filePath.Substring(startFileNamePosition + 1, nameLenth);
+
+            return name;
+        }
+
+        public void saveFile()
+        {
+            string fileName = string.Empty;
+
+            if (haveFileName == true)
+            {
+                System.IO.StreamWriter fs = System.IO.File.CreateText(filePath);
+                fs.WriteLine(content.Text);
+                fs.Close();
+                modified = false;
+            }
+            else
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog1.FileName;
+                    System.IO.StreamWriter fs = System.IO.File.CreateText(filePath);
+                    fs.WriteLine(content.Text);
+                    fs.Close();
+                    modified = false;
+                    haveFileName = true;
+                }
+                else
+                {
+                    memoPadForm.ActiveForm.Text = "제목 없음 - 메모장";
+                    return;
+                }
+            }
+
+            fileName = getFileName();
+            memoPadForm.ActiveForm.Text = fileName + " - 메모장";
+        }
+
+        private void content_TextChanged(object sender, EventArgs e)
+        {
+            modified = true;
+        }
+
+        private void newPage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(modified == true)
+                {
+                    DialogResult result = MessageBox.Show("변경내역을 저장하시겠습니까?", "저장", MessageBoxButtons.YesNoCancel );
+                    if(result == DialogResult.Yes)
+                    {
+                        saveFile();
+                        reset_memo();
+                    }
+                    else if(result == DialogResult.No)
+                    {
+                        reset_memo();
+                    }
+                    else if(result == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    if(haveFileName == true)
+                    {
+                        reset_memo();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("새 파일을 준비하는 도중 이상이 발생했습니다.", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            string fileName = string.Empty;
+
+            try
+            {
+                if(modified == true)
+                {
+                    DialogResult result = MessageBox.Show("변경 내역을 저장 하시겠습니까? ","저장",MessageBoxButtons.YesNo);
+                    if(result == DialogResult.Yes)
+                    {
+                        saveFile();
+                        MessageBox.Show("저장완료.");
+                    }
+                    reset_memo();
+                }
+                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                    fileName = getFileName();
+                    memoPadForm.ActiveForm.Text = fileName + " - 메모장 ";                    
+
+                    System.IO.StreamReader fs = System.IO.File.OpenText(filePath);
+                    content.Text = fs.ReadToEnd();
+                    fs.Close();
+                    modified = false;
+                    haveFileName = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("열기를 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFile();
+            }
+            catch
+            {
+                MessageBox.Show("저장을 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(modified == true)
+                {
+                    DialogResult result = MessageBox.Show("변경 사항을 저장하시겠습니까?","저장", MessageBoxButtons.YesNoCancel);
+                    if(result == DialogResult.Yes)
+                    {
+                        saveFile();
+                    }
+                    else if(result == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                    reset_memo();
+                }
+                Application.Exit();
+            }
+            catch
+            {
+                MessageBox.Show("종료 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons. OK,MessageBoxIcon.Warning);
+            }
+        }
+
+        private void anotherName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                haveFileName = false;
+                saveFile();
+            }
+            catch
+            {
+                MessageBox.Show("저장을 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void memoPadInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is c# study.\n It's beginner level~");
+        }
+    }
+}
